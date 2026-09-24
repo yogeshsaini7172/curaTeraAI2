4af17f8d5796cc905aa2c191055518e2f7e16e5d
Yogesh Saini <yogeshsaini7172@gmail.com>
2026-09-24 20:53:29 +0530

    implemented profile section

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 
 auth_bp = Blueprint('auth', __name__)
 chat_bp = Blueprint('chat', __name__)
+profile_bp = Blueprint('profile', __name__)
 
 from . import auth
 from . import chat
+from . import profile
